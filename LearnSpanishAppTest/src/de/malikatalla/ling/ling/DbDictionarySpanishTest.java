33af86d4f6ad1b48ec380690510e87d7f2c8f7f7
HerrAachen <MalikAtalla@hotmail.com>
2014-03-13 20:58:19 -0700

    test case for conjugations

---
+++ LearnSpanishAppTest/src/de/malikatalla/ling/ling/DbDictionarySpanishTest.java
@@ -0,0 +1,39 @@
+package de.malikatalla.ling.ling;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.malikatalla.ling.MainActivity;
+
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+
+public class DbDictionarySpanishTest extends ActivityUnitTestCase<MainActivity> {
+
+	public DbDictionarySpanishTest(){
+		super(MainActivity.class);
+	}
+	
+	public DbDictionarySpanishTest(Class<MainActivity> activityClass) {
+		super(activityClass);
+	}
+
+	private DbDictionary d;
+	
+	@Before
+	public void setUp() throws Exception{
+		super.setUp();
+        Intent mLaunchIntent = new Intent(getInstrumentation()
+                .getTargetContext(), MainActivity.class);
+
+		startActivity(mLaunchIntent, null, null);
+		d= new DbDictionarySpanish(getActivity());
+	}
+	
+	@Test
+	public void testInflections() {
+		assertNull(d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, null));
+		assertEquals("advierto", d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
+		assertEquals("advertí", d.getInflectedForm("advertir", Tense.SIMPLE_PAST, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
+	}
+}
