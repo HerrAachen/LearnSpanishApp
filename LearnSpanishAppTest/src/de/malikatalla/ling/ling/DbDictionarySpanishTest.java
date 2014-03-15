563ab8406597a35817e860e3b68ec7a89cac7491
HerrAachen <MalikAtalla@hotmail.com>
2014-03-15 13:49:07 -0700

    Formatting

--- LearnSpanishAppTest/src/de/malikatalla/ling/ling/DbDictionarySpanishTest.java
+++ LearnSpanishAppTest/src/de/malikatalla/ling/ling/DbDictionarySpanishTest.java
@@ -10,30 +10,29 @@ import android.test.ActivityUnitTestCase;
 
 public class DbDictionarySpanishTest extends ActivityUnitTestCase<MainActivity> {
 
-	public DbDictionarySpanishTest(){
-		super(MainActivity.class);
-	}
-	
-	public DbDictionarySpanishTest(Class<MainActivity> activityClass) {
-		super(activityClass);
-	}
+  public DbDictionarySpanishTest() {
+    super(MainActivity.class);
+  }
 
-	private DbDictionary d;
-	
-	@Before
-	public void setUp() throws Exception{
-		super.setUp();
-        Intent mLaunchIntent = new Intent(getInstrumentation()
-                .getTargetContext(), MainActivity.class);
+  public DbDictionarySpanishTest(Class<MainActivity> activityClass) {
+    super(activityClass);
+  }
 
-		startActivity(mLaunchIntent, null, null);
-		d= new DbDictionarySpanish(getActivity());
-	}
-	
-	@Test
-	public void testInflections() {
-		assertNull(d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, null));
-		assertEquals("advierto", d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
-		assertEquals("advertí", d.getInflectedForm("advertir", Tense.SIMPLE_PAST, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
-	}
+  private DbDictionary d;
+
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+    Intent mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+
+    startActivity(mLaunchIntent, null, null);
+    d = new DbDictionarySpanish(getActivity());
+  }
+
+  @Test
+  public void testInflections() {
+    assertNull(d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, null));
+    assertEquals("advierto", d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
+    assertEquals("advertí", d.getInflectedForm("advertir", Tense.SIMPLE_PAST, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
+  }
 }
