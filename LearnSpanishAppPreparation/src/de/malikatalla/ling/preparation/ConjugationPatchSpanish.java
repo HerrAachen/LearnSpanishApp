6f2afebbe7b8473bd7464903b1d9b26c9488bcdd
HerrAachen <MalikAtalla@hotmail.com>
2014-06-17 08:24:53 +0200

    Correction of reflexive conjugation in db creation

--- LearnSpanishAppPreparation/src/de/malikatalla/ling/preparation/ConjugationPatchSpanish.java
+++ LearnSpanishAppPreparation/src/de/malikatalla/ling/preparation/ConjugationPatchSpanish.java
@@ -1,6 +1,8 @@
 package de.malikatalla.ling.preparation;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import de.malikatalla.ling.Global;
 import de.malikatalla.ling.ling.Flection;
@@ -19,23 +21,31 @@ public class ConjugationPatchSpanish implements ConjugationPatch {
 
   public void applyPatch(Map<String, ConjugationDescription> conjugations) {
     ConjugationDescription haber = conjugations.get("haber");
-    haber.getIrregularFlections().addInflectedForm(
-        new Flection(Tense.PRESENT, Person.THIRD, Number.SINGULAR, null, Mode.INDICATIVE), "ha");
+    haber.getIrregularFlections().addInflectedForm(new Flection(Tense.PRESENT, Person.THIRD, Number.SINGULAR, null, Mode.INDICATIVE), "ha");
     correctReflexiveConjugations(conjugations);
   }
 
   /**
-   * When there is a reflexive verb with non reflexive regular conjugations,
-   * but reflexive regular conjugation exists, prefer the latter.
+   * When there is a reflexive verb with non reflexive regular conjugations, but
+   * reflexive regular conjugation exists, prefer the latter.
    */
   private void correctReflexiveConjugations(Map<String, ConjugationDescription> conjugations) {
+    //collect all regular conjugation names
+    Set<String> regConj = new HashSet<>();
+    for (Map.Entry<String, ConjugationDescription> entry : conjugations.entrySet()) {
+      regConj.add(entry.getValue().getBasicConjugation());
+    }
     LingUtil lingUtil = Global.getLingUtil();
-    for(Map.Entry<String,ConjugationDescription> entry: conjugations.entrySet()){
+    for (Map.Entry<String, ConjugationDescription> entry : conjugations.entrySet()) {
       String verb = entry.getKey();
-      if (lingUtil.isReflexive(verb)){
+      if (lingUtil.isReflexive(verb)) {
         ConjugationDescription conj = entry.getValue();
-        if (!conj.getBasicConjugation().endsWith("se")){
-        //TODO
+        if (!conj.getBasicConjugation().endsWith("se")) {
+          String reflexiveConjugation = conj.getBasicConjugation() + "se";
+          if (regConj.contains(reflexiveConjugation)){
+            System.out.println("Changing conjugation of " + verb + " from " + conj.getBasicConjugation() + " to " + reflexiveConjugation);
+            conj.setBasicConjugation(reflexiveConjugation);
+          }
         }
       }
     }
--- LearnSpanishAppTest/src/de/malikatalla/ling/ling/DbDictionarySpanishTest.java
+++ LearnSpanishAppTest/src/de/malikatalla/ling/ling/DbDictionarySpanishTest.java
@@ -31,7 +31,7 @@ public class DbDictionarySpanishTest extends ActivityUnitTestCase<MainActivity> 
 
   public void testVerbs() {
     List<String> verbs = d.getAllVerbs();
-    assertTrue(verbs.size()>3000);
+    assertTrue(verbs.size() > 3000);
     for (String verb : verbs) {
       assertFalse(verb.matches("\\p{Punct}"));
     }
@@ -40,19 +40,15 @@ public class DbDictionarySpanishTest extends ActivityUnitTestCase<MainActivity> 
   public void testIndicative() {
     assertNull(d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, null));
     assertEquals("advierto", d.getInflectedForm("advertir", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
-    assertEquals("advertí",
-        d.getInflectedForm("advertir", Tense.SIMPLE_PAST, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
-    assertEquals("advertías",
-        d.getInflectedForm("advertir", Tense.IMPERFECT, Person.SECOND, Number.SINGULAR, null, Mode.INDICATIVE));
+    assertEquals("advertí", d.getInflectedForm("advertir", Tense.SIMPLE_PAST, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
+    assertEquals("advertías", d.getInflectedForm("advertir", Tense.IMPERFECT, Person.SECOND, Number.SINGULAR, null, Mode.INDICATIVE));
     assertEquals("caminaréis", d.getInflectedForm("caminar", Tense.FUTURE, Person.SECOND, Number.PLURAL, null, Mode.INDICATIVE));
-    assertEquals("intentarían",
-        d.getInflectedForm("intentar", Tense.CONDITIONAL, Person.THIRD, Number.PLURAL, null, Mode.INDICATIVE));
+    assertEquals("intentarían", d.getInflectedForm("intentar", Tense.CONDITIONAL, Person.THIRD, Number.PLURAL, null, Mode.INDICATIVE));
   }
 
   public void testSubjunctive() {
     assertEquals("falle", d.getInflectedForm("fallar", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, Mode.SUBJUNCTIVE));
-    assertEquals("alzáramos/alzásemos",
-        d.getInflectedForm("alzar", Tense.IMPERFECT, Person.FIRST, Number.PLURAL, null, Mode.SUBJUNCTIVE));
+    assertEquals("alzáramos/alzásemos", d.getInflectedForm("alzar", Tense.IMPERFECT, Person.FIRST, Number.PLURAL, null, Mode.SUBJUNCTIVE));
     assertEquals("alzáremos", d.getInflectedForm("alzar", Tense.FUTURE, Person.FIRST, Number.PLURAL, null, Mode.SUBJUNCTIVE));
     assertEquals("coma", d.getInflectedForm("comer", Tense.PRESENT, Person.FIRST, Number.SINGULAR, null, Mode.SUBJUNCTIVE));
   }
@@ -62,31 +58,40 @@ public class DbDictionarySpanishTest extends ActivityUnitTestCase<MainActivity> 
   }
 
   public void testIndicativeReflexive() {
-    assertEquals("te apasionas",
-        d.getInflectedForm("apasionarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.INDICATIVE));
-    assertEquals("se apasionó",
-        d.getInflectedForm("apasionarse", Tense.SIMPLE_PAST, Person.THIRD, Number.SINGULAR, null, Mode.INDICATIVE));
+    assertEquals("te apasionas", d.getInflectedForm("apasionarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.INDICATIVE));
+    assertEquals("se apasionó", d.getInflectedForm("apasionarse", Tense.SIMPLE_PAST, Person.THIRD, Number.SINGULAR, null, Mode.INDICATIVE));
   }
 
   public void testImperativeReflexive() {
     assertEquals("acalora", d.getInflectedForm("acalorar", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
-    assertEquals("acalórate",
-        d.getInflectedForm("acalorarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
+    assertEquals("acalórate", d.getInflectedForm("acalorarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
     assertEquals("acaloremos", d.getInflectedForm("acalorar", Tense.PRESENT, Person.FIRST, Number.PLURAL, null, Mode.IMPERATIVE));
-    assertEquals("acalorémonos",
-        d.getInflectedForm("acalorarse", Tense.PRESENT, Person.FIRST, Number.PLURAL, null, Mode.IMPERATIVE));
+    assertEquals("acalorémonos", d.getInflectedForm("acalorarse", Tense.PRESENT, Person.FIRST, Number.PLURAL, null, Mode.IMPERATIVE));
     assertEquals("acalorad", d.getInflectedForm("acalorar", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
-    assertEquals("acaloraos",
-        d.getInflectedForm("acalorarse", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
-    assertEquals("abarragánate",
-        d.getInflectedForm("abarraganarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
-    assertEquals("abarraganémonos",
-        d.getInflectedForm("abarraganarse", Tense.PRESENT, Person.FIRST, Number.PLURAL, null, Mode.IMPERATIVE));
+    assertEquals("acaloraos", d.getInflectedForm("acalorarse", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
+    assertEquals("abarragánate", d.getInflectedForm("abarraganarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
+    assertEquals("abarraganémonos", d.getInflectedForm("abarraganarse", Tense.PRESENT, Person.FIRST, Number.PLURAL, null, Mode.IMPERATIVE));
+  }
+  
+
+  public void testImperativeReflexiveApoyarse() {
+    assertEquals("apóyate", d.getInflectedForm("apoyarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
+  }
+  
+  public void testImperativeReflexiveAproximarse() {
+    assertEquals("aproxímate", d.getInflectedForm("aproximarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
+    assertEquals("aproximaos", d.getInflectedForm("aproximarse", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
+    assertEquals("aproxímense", d.getInflectedForm("aproximarse", Tense.PRESENT, Person.THIRD, Number.PLURAL, null, Mode.IMPERATIVE));
+  }
+  
+  public void testImperativeReflexiveRepanchingarse() {
+    assertEquals("repanchíngate", d.getInflectedForm("repanchingarse", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.IMPERATIVE));
+    assertEquals("repanchinguémonos", d.getInflectedForm("repanchingarse", Tense.PRESENT, Person.FIRST, Number.PLURAL, null, Mode.IMPERATIVE));
+    assertEquals("repanchínguense", d.getInflectedForm("repanchingarse", Tense.PRESENT, Person.THIRD, Number.PLURAL, null, Mode.IMPERATIVE));
   }
 
   public void testIndicativePastPerfect() {
-    assertEquals("he esperado",
-        d.getInflectedForm("esperar", Tense.PAST_PERFECT, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
+    assertEquals("he esperado", d.getInflectedForm("esperar", Tense.PAST_PERFECT, Person.FIRST, Number.SINGULAR, null, Mode.INDICATIVE));
   }
 
   public void testIrregular() {
@@ -96,38 +101,41 @@ public class DbDictionarySpanishTest extends ActivityUnitTestCase<MainActivity> 
     assertEquals("estás", d.getInflectedForm("estar", Tense.PRESENT, Person.SECOND, Number.SINGULAR, null, Mode.INDICATIVE));
     assertEquals("ha", d.getInflectedForm("haber", Tense.PRESENT, Person.THIRD, Number.SINGULAR, null, Mode.INDICATIVE));
   }
-  
-  public void testAprenderse(){
+
+  public void testAprenderse() {
     Dictionary allInflectedForms = d.getAllInflectedForms("aprenderse");
     assertEquals("aprendaos", d.getInflectedForm("aprenderse", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
-    assertEquals("aprendaos", allInflectedForms.getInflectedForm("aprenderse", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
+    assertEquals("aprendaos",
+        allInflectedForms.getInflectedForm("aprenderse", Tense.PRESENT, Person.SECOND, Number.PLURAL, null, Mode.IMPERATIVE));
   }
 
-//  public void testCoverage() throws Exception {
-//    int total = 0;
-//    int covered = 0; strom.berlin@vattenv.de
-//    int verbCount = 0;
-//    List<String> allVerbs = d.getAllVerbs();
-//    for (String verb : allVerbs) {
-//      try {
-//      Dictionary allInflectedForms = d.getAllInflectedForms(verb);
-//      Log.i(Global.DEBUG, ++verbCount + "/" + allVerbs.size());
-//      for (Flection f : Global.getColumnConverter().flectionIterator()) {
-//        total++;
-//        String inflectedForm = allInflectedForms.getInflectedForm(verb, f.getTense(), f.getPerson(), f.getNumber(), f.getGender(), f.getMode());
-//        if (inflectedForm != null) {
-//          covered++;
-//        }
-//      }
-//      } catch (Exception e){
-//        Log.i(Global.DEBUG, "test coverage: " + verb);
-//        throw e;
-//      }
-//    }
-//    String message = "Coverage of inflected forms: " + covered + "/" + total + " (" + (covered * 100.0) / total + "%)";
-//    Log.i(Global.DEBUG, message);
-//    if (total != covered) {
-//      fail(message);
-//    }
-//  }
+  // public void testCoverage() throws Exception {
+  // int total = 0;
+  // int covered = 0; strom.berlin@vattenv.de
+  // int verbCount = 0;
+  // List<String> allVerbs = d.getAllVerbs();
+  // for (String verb : allVerbs) {
+  // try {
+  // Dictionary allInflectedForms = d.getAllInflectedForms(verb);
+  // Log.i(Global.DEBUG, ++verbCount + "/" + allVerbs.size());
+  // for (Flection f : Global.getColumnConverter().flectionIterator()) {
+  // total++;
+  // String inflectedForm = allInflectedForms.getInflectedForm(verb,
+  // f.getTense(), f.getPerson(), f.getNumber(), f.getGender(), f.getMode());
+  // if (inflectedForm != null) {
+  // covered++;
+  // }
+  // }
+  // } catch (Exception e){
+  // Log.i(Global.DEBUG, "test coverage: " + verb);
+  // throw e;
+  // }
+  // }
+  // String message = "Coverage of inflected forms: " + covered + "/" + total +
+  // " (" + (covered * 100.0) / total + "%)";
+  // Log.i(Global.DEBUG, message);
+  // if (total != covered) {
+  // fail(message);
+  // }
+  // }
 }
